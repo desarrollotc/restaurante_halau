7db04aebc2e26b1d5975ec09b66f6b5861d52773
Yeferson Andres Rios Martinez <desarrollos5@cvida.local>
2024-02-13 10:51:30 -0500

    Cambio en vista cleinte

--- views/contents/cliente-view.php
+++ views/contents/cliente-view.php
@@ -6,6 +6,7 @@ $cons2 = $cons->Listar_menu_Controller();
 //  print_r($cons2);
 // echo date('Y-m-d');
 ?>
+
 <form class="FormularioAjax" method="post" action="<?php echo SERVERURL ?>ajax/OrdenAjax.php">
   <div class="container height75">
     <div class="containerForm containerGap30">
@@ -17,19 +18,26 @@ $cons2 = $cons->Listar_menu_Controller();
         <label for="campo2">Código de verificación</label>
         <input type="password" name="campo2" class="form-control" id="exampleInputPassword1" placeholder="*******">
       </div> -->
-                      <input type="hidden" class="form-control" id="numero_cliente_orden" name="numero_cliente_orden"
-                        value="<?php print $_SESSION['numero_spm'];?>">
-                        <input type="hidden" class="form-control" id="correo_cliente" name="correo_cliente"
-                        value="<?php print $_SESSION['correo_spm'];?>">
-                        <input type="hidden" class="form-control" id="id_usuario" name="id_usuario"
-                        value="<?php print $_SESSION['id_spm'];?>">
+      <input type="hidden" class="form-control" id="numero_cliente_orden" name="numero_cliente_orden" value="<?php print $_SESSION['numero_spm']; ?>">
+      <input type="hidden" class="form-control" id="correo_cliente" name="correo_cliente" value="<?php print $_SESSION['correo_spm']; ?>">
+      <input type="hidden" class="form-control" id="id_usuario" name="id_usuario" value="<?php print $_SESSION['id_spm']; ?>">
       <div class="inputContainer">
         <label for="">Menú deseado</label>
-        <select name="menu_orden"  class="form-select"  aria-label="Default select example" required>
+        <select name="menu_orden" class="form-select" aria-label="Default select example" required>
           <option value="" selected>Seleccione menú deseado</option>
           <?php foreach ($cons2 as $value) { ?>
             <option value="<?php print $value['id_menu']; ?>"><?php print $value['nombre_menu']; ?></option>
           <?php } ?>
+        </select>
+      </div>
+      <div class="inputContainer">
+        <label for="">Área</label>
+        <select name="menu_orden" class="form-select selectSearch" aria-label="Default select example" required>
+          <option value="" selected>Seleccione área perteneciente</option>
+          <option value=""> Tecnología</option>
+          <option value=""> Cirugía</option>
+          <option value=""> Contabilidad</option>
+          <option value=""> Recursos humanos</option>
         </select>
       </div>
       <!-- Submit button -->
