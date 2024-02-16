1379305726ca74f2289de489616cf19a9bc7913f
Yeferson Andres Rios Martinez <desarrollos5@cvida.local>
2024-02-16 16:26:55 -0500

    Actualizacion de vistas

--- views/contents/menu-view.php
+++ views/contents/menu-view.php
@@ -1,17 +1,21 @@
-<?php 
-   require_once './config/APP.php'; 
-   require_once './controllers/MenuController.php';
+<?php
+require_once './config/APP.php';
+require_once './controllers/MenuController.php';
 
-   ?>
-<table class="table" id="tabla_inc">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">Menú</th>
-      <th scope="col">Precio</th>
-      <th scope="col">Cantidad</th>
-      <th scope="col">Estado</th>
-      <th scope="col">Accion</th>
-    </tr>
-  </thead>
-</table>
+?>
+<div class="containerPedido">
+  <div class="containerTable">
+    <table class="table wdhMax" id="tabla_inc">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">Menú</th>
+          <th scope="col">Precio</th>
+          <th scope="col">Cantidad</th>
+          <th scope="col">Estado</th>
+          <th scope="col">Accion</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</div>
